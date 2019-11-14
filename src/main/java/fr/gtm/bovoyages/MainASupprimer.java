77890cb4e09a8863853b7f6e426f11c1ee982aaf
jcfag2 <j.fagothey@esff.fr>
2019-11-14 18:42:00 +0100

    COUCOU C'EST MOI

---
+++ src/main/java/fr/gtm/bovoyages/MainASupprimer.java
@@ -0,0 +1,10 @@
+package fr.gtm.bovoyages;
+
+public class MainASupprimer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientBovoyagesServices.getAllRegions();
+	}
+
+}
